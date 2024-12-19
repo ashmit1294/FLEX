72a2b4c2194cd2f1054744526e9f73ae9c4044c7
Ashmit Nigam <ashmit.nigam1294@gmail.com>
2024-12-19 19:45:21 +0530

    Working Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-const App = () => {
+const App=()=> {
+
   return (
-    <div className="">
-      Hello
-    </div>
+    <>
+    <p className='text-red-600 text-3xl'>
+    Click on the Vite and React logos to learn more
+    </p>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{html,js,ts,tsx}"],
+  content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
     extend: {},
   },
